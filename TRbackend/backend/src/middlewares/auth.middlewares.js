a5aae47cc6373984b9ed45ca6221d0ff0cee0123
Kaustubh <kaustubhjaitapkar2004@gmail.com>
2024-09-26 18:56:06 +0530

    updated url

--- TRbackend/backend/src/middlewares/auth.middlewares.js
+++ TRbackend/backend/src/middlewares/auth.middlewares.js
@@ -9,6 +9,8 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
+
+        console.log("token" + token)
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
