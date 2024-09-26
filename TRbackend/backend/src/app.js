365fb05e56a48712677e5bacd293bd174c7e5e99
Kaustubh <kaustubhjaitapkar2004@gmail.com>
2024-09-26 18:29:14 +0530

    updated url

--- TRbackend/backend/src/app.js
+++ TRbackend/backend/src/app.js
@@ -3,11 +3,16 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express()
+let allowUrls = "*"
 
-app.use(cors({
-    origin:'http://localhost:5173',
+app.use(
+  cors({
+    origin: "https://issue-tracker-system.vercel.app",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
     credentials: true,
-}))
+    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+  })
+);
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
