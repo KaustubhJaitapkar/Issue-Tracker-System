3c1139116f0ccfb63618babab155de4f371aaa6c
Kaustubh <kaustubhjaitapkar2004@gmail.com>
2024-10-01 17:06:25 +0530

    Options to register departments

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -83,9 +83,9 @@ function Register() {
             className="w-full px-2 py-1 sm:px-3 sm:py-2 md:px-4 md:py-3 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
           />
           <select
-            value={formData.requireDepartment}
             onChange={handleChange}
-            name="requireDepartment"
+            name="department"
+            value={formData.department}
             className="w-full mb-3 sm:mb-4 p-2 sm:p-3 border border-gray-300 rounded-lg text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             <option>Select a Department</option>
